1dc07f444c70303e86563d3b3f169948fb6cd62a
Glen Barnhardt <gbarnhardt@gmail.com>
2022-11-18 18:29:09 -0500

    update terraform config and dockerfile

---
+++ terraform/registry/config/backend-config.tf
@@ -0,0 +1,5 @@
+bucket                      = "terraform-states-bei"
+key                         = "ci-cd-bei.tfstate"
+region                      = "us-east-1"
+encrypt                     = true
+dynamodb_table              = "terraform-locks"
